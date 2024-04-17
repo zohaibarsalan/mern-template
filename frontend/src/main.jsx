cd685f9c7d9a752b5ea360849f82d67a56bf516a
Zohaib Arsalan <zohaibhstart@gmail.com>
2024-04-17 19:32:23 +0500

    Tailwind css login pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { Toaster } from 'sonner';
 import './index.css';
-import { ThemeProvider } from 'next-themes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider forcedTheme={'dark' || undefined} attribute="class">
-      <App />
-      <Toaster position="top-right" richColors closeButton />
-    </ThemeProvider>
+    <App />
+    <Toaster position="top-right" richColors closeButton />
   </React.StrictMode>
 );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,6 @@
 export default {
   mode: 'jit',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
   theme: {
     extend: {},
   },
